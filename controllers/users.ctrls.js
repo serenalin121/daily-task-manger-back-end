45179acf75a6baca9c90afb4d3581fad20218a69
serenalin121 <serenalin121@gmail.com>
2021-10-27 14:45:39 -0700

    passed the session.currentUser infor from back-end to front end

--- controllers/users.ctrls.js
+++ controllers/users.ctrls.js
@@ -1,47 +1,57 @@
-const db = require('../models')
-const bcrypt = require('bcrypt')
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
 const signup = (req, res) => {
-  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
   db.User.create(req.body, (error, createdUser) => {
     if (error) {
-      res.status(400).json({error: error.message})
+      res.status(400).json({ error: error.message });
     } else {
-      console.log(createdUser)
-      res.status(200).json(createdUser)
+      // console.log(createdUser);
+      req.session.currentUser = createdUser;
+      // console.log(req.session.currentUser);
+      res.status(200).json(createdUser);
     }
-  })
-}
+  });
+};
 
 const signin = (req, res) => {
-  db.User.findOne({username: req.body.username}, (error, foundUser) => {
+  db.User.findOne({ username: req.body.username }, (error, foundUser) => {
     if (error) {
-      res.send(error)
+      res.send(error);
     } else {
       if (foundUser) {
-        console.log(req.body.password)
-        console.log(foundUser.password)
+        // console.log(req.body.password);
+        // console.log(foundUser.password);
         if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-          req.session.currentUser = foundUser
-          res.status(200).json(foundUser)
+          req.session.currentUser = foundUser;
+          res.status(200).json(foundUser);
         } else {
-          res.status(404).json({error: 'Incorrect Password or Username...'})
+          res.status(404).json({ error: "Incorrect Password or Username..." });
         }
       } else {
-        res.status(400).json({error: error})
+        res.status(400).json({ error: error });
       }
     }
-  })
-}
+  });
+};
+
+const renew = (req, res) => {
+  res.status(200).json({ result: !!req.session.currentUser });
+};
 
 const signout = (req, res) => {
   req.session.destroy(() => {
-    res.status(200).json({msg: 'User signed out'})
-  })
-}
+    res.status(200).json({ msg: "User signed out" });
+  });
+};
 
 module.exports = {
+  renew,
   signup,
   signin,
   signout,
-}
+};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const ctrls = require('../controllers')
+const ctrls = require("../controllers");
 
-router.post('/signup', ctrls.users.signup)
-router.post('/signin', ctrls.users.signin)
-router.delete('/signout', ctrls.users.signout)
+router.post("/signup", ctrls.users.signup);
+router.post("/signin", ctrls.users.signin);
+router.delete("/signout", ctrls.users.signout);
+router.post("/renew", ctrls.users.renew);
 
-module.exports = router
+module.exports = router;
